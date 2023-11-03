08dfc2234e2e587967383e1fb3d7cebc1f95d6e6
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-11-03 23:39:57 +0500

    fix linters error

--- main_file.rb
+++ main_file.rb
@@ -1,31 +1,31 @@
 MORSE_CODE = {
-  ".-" => "A",
-  "-..." => "B",
-  "-.-." => "C",
-  "-.." => "D",
-  "." => "E",
-  "..-." => "F",
-  "--." => "G",
-  "...." => "H",
-  ".." => "I",
-  ".---" => "J",
-  "-.-" => "K",
-  ".-.." => "L",
-  "--" => "M",
-  "-." => "N",
-  "---" => "O",
-  ".--." => "P",
-  "--.-" => "Q",
-  ".-." => "R",
-  "..." => "S",
-  "-" => "T",
-  "..-" => "U",
-  "...-" => "V",
-  ".--" => "W",
-  "-..-" => "X",
-  "-.--" => "Y",
-  "--.." => "Z"
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 # Method to decode a Morse code character
 def decode_char(morse_char)
@@ -34,14 +34,14 @@ end
 
 # Method to decode an entire word in Morse code
 def decode_word(morse_word)
-  morse_word.split(" ").map { |char| decode_char(char) }.join
+  morse_word.split.map { |char| decode_char(char) }.join
 end
 
 # Method to decode the entire message in Morse code
 def decode_message(morse_message)
-  morse_message.split("   ").map { |word| decode_word(word) }.join(" ")
+  morse_message.split('   ').map { |word| decode_word(word) }.join(' ')
 end
 
-message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_msg = decode_message(message)
 puts decoded_msg
